69028d6746293d953d472291121e296aae4363e6
qwa-matheusilvac <matheus.correia@qwasolucoes.com.br>
2025-04-14 14:18:57 -0300

    feat: MainView.fxml

--- src/main/java/github/matheusilvac/workshopjavafx/HelloApplication.java
+++ src/main/java/github/matheusilvac/workshopjavafx/HelloApplication.java
@@ -2,6 +2,7 @@ package github.matheusilvac.workshopjavafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Parent parent = FXMLLoader.load(getClass().getResource("/github/matheusilvac/workshopjavafx/gui/MainView.fxml"));
+        Scene scene = new Scene(parent);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
